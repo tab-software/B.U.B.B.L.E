ed9895fe64c995f2035e241441783060e991f3da
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-01-25 10:30:28 -0300

    Basic player movement implementation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,27 +1,54 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float velocity = 1.0f;
-    public float rotationVelocity = 30.0f;
-    public float maxAngleRotation = 45.0f;
+    public float rotationVelocity = 50.0f;
+    public float maxAngleRotation = 30.0f;
+    public float swimmingEffectAmplitude = 1.0f; 
+    public float swimmingEffectFrecuency = 1.0f; 
+
+    private float initialYPosition = 0.0f;
+    private float angle = 0.0f;
 
     void Start()
     {
-        
+        this.initialYPosition = this.GetComponent<Transform>().position.y;
     }
 
     void Update()
     {
         float Xmovement = Input.GetAxis("Horizontal");
-        float Ymovement = Input.GetAxis("Vertical");
 
-        this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(Xmovement, Ymovement) * this.velocity;
-        if(
-            (this.GetComponent<Transform>().eulerAngles.z < maxAngleRotation & Xmovement > 0)
-        ||  (this.GetComponent<Transform>().eulerAngles.z > -maxAngleRotation & Xmovement < 0)
-          )
-        this.GetComponent<Transform>().eulerAngles = new Vector3(0f, 0f, this.GetComponent<Transform>().eulerAngles.z - Xmovement * Time.deltaTime * this.rotationVelocity);
+        this.GetComponent<Transform>().position = new Vector3(this.GetComponent<Transform>().position.x,
+                                                              this.initialYPosition + swimmingEffectAmplitude*(float)Math.Cos(2.0*Math.PI*swimmingEffectFrecuency*Time.time),
+                                                              this.GetComponent<Transform>().position.z);
+
+        this.angle = transform.eulerAngles.z;
+        if (this.angle > 180) this.angle -= 360;
+
+        this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(Xmovement, 0) * this.velocity;
+        
+        if (Xmovement != 0.0f)
+        {
+            if((this.angle < maxAngleRotation && Xmovement < 0)
+            || (this.angle > -maxAngleRotation && Xmovement > 0))
+            {
+                this.GetComponent<Transform>().eulerAngles = new Vector3(0f, 0f, this.GetComponent<Transform>().eulerAngles.z - Xmovement * Time.deltaTime * this.rotationVelocity);
+            }
+        }
+        else
+        {
+            if(this.angle > 0.0f)
+            {
+                this.GetComponent<Transform>().eulerAngles = new Vector3(0f, 0f, this.GetComponent<Transform>().eulerAngles.z - Time.deltaTime * this.rotationVelocity);
+            }
+            if(this.angle < 0.0f)
+            {
+                this.GetComponent<Transform>().eulerAngles = new Vector3(0f, 0f, this.GetComponent<Transform>().eulerAngles.z + Time.deltaTime * this.rotationVelocity);
+            }
+        }
     }
 }
