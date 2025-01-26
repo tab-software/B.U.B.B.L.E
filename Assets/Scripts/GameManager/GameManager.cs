991d9b52b6de5e0e7f6ef0b40983f20242bd2bd6
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-01-26 16:20:03 -0300

    Finish progress

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
     public GameObject logoBar;
     public GameObject progressBar;
     private float distBar = 0.0f;
+    public GameObject backgroundGradient;
     void Start()
     {
         this.distBar = this.endBar.transform.position.y - this.initBar.transform.position.y;
@@ -54,10 +56,16 @@ public class GameManager : MonoBehaviour
                     Time.timeScale = 0.0f;    
                 }
             }
-            float y = this.distBar * ((Time.time - initilizedAt)/levelDuration);
+            float progress = ((Time.time - initilizedAt)/levelDuration);
+            float y = this.distBar * progress;
             this.logoBar.transform.position = new Vector3(this.logoBar.transform.position.x,
                                                           this.initBar.transform.position.y + y,
                                                           this.logoBar.transform.position.z);
+            Vector4 color = new Vector4(1.0f, 1.0f, 1.0f);
+            color.x = (1.0f/255.0f) * (32 * Math.Max(1.0f - progress, 0.0f));
+            color.y = (1.0f/255.0f) * (54 + 136 * Math.Min(progress, 1.0f));
+            color.z = (1.0f/255.0f) * (87 + 168 * Math.Min(progress, 1.0f));
+            this.backgroundGradient.GetComponent<Renderer>().material.SetColor("_ColorA", color);
         }
     }
 }
