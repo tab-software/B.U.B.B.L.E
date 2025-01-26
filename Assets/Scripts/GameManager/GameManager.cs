7e47a6db2ed00f5f82a2066d575c5e3b50c667ac
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-01-26 16:00:44 -0300

    progress bar

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,16 +5,26 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> spawners;
     public bool initilized = false;
+    public float levelDuration = 60.0f;
+    private float initilizedAt = 0.0f;
     public GameObject shottedBox;
     public GameObject PauseGameObject;
+
+    public GameObject initBar;
+    public GameObject endBar;
+    public GameObject logoBar;
+    public GameObject progressBar;
+    private float distBar = 0.0f;
     void Start()
     {
-        
+        this.distBar = this.endBar.transform.position.y - this.initBar.transform.position.y;
+        this.logoBar.transform.position = this.initBar.transform.position;
+        this.progressBar.SetActive(false);
+        Debug.Log(this.distBar);
     }
 
     void Update()
     {
-
         if(!this.initilized)
         {
             if(shottedBox.GetComponent<BubbleableObject>().bubbled)
@@ -23,22 +33,31 @@ public class GameManager : MonoBehaviour
                 {
                     spawner.GetComponent<Spawner>().Enable();
                 }
+                this.progressBar.SetActive(true);
+                this.initilizedAt = Time.time;
                 GameObject.Find("SuperFish").GetComponent<SuperFish>().directionY = -1.0f;
                 this.initilized = true;
             }
         }
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(this.initilized)
         {
-            if(this.PauseGameObject.activeSelf)
+            if(Input.GetKeyDown(KeyCode.Escape))
             {
-                this.PauseGameObject.SetActive(false);
-                Time.timeScale = 1.0f;
+                if(this.PauseGameObject.activeSelf)
+                {
+                    this.PauseGameObject.SetActive(false);
+                    Time.timeScale = 1.0f;
+                }
+                else
+                {
+                    this.PauseGameObject.SetActive(true);
+                    Time.timeScale = 0.0f;    
+                }
             }
-            else
-            {
-                this.PauseGameObject.SetActive(true);
-                Time.timeScale = 0.0f;    
-            }
+            float y = this.distBar * ((Time.time - initilizedAt)/levelDuration);
+            this.logoBar.transform.position = new Vector3(this.logoBar.transform.position.x,
+                                                          this.initBar.transform.position.y + y,
+                                                          this.logoBar.transform.position.z);
         }
     }
 }
