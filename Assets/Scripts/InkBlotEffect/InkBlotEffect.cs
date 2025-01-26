f0db55a0310af73ac811dc7d08344d8f94691cdf
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-01-26 15:30:02 -0300

    control point

---
+++ Assets/Scripts/InkBlotEffect/InkBlotEffect.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class InkBlot : MonoBehaviour
+{
+    public float velocity = 10.0f;
+
+    private void Start()
+    {
+        this.transform.localScale = new Vector3(0f, 0f, 0f);
+    }
+
+    private void Update()
+    {
+        if(this.transform.localScale.x < 4.0f)
+            this.transform.localScale = new Vector3(
+                this.transform.localScale.x + velocity * Time.deltaTime,
+                this.transform.localScale.y + velocity * Time.deltaTime,
+                this.transform.localScale.z
+            );
+        else
+            this.GetComponent<SpriteRenderer>().color = new Vector4(
+                this.GetComponent<SpriteRenderer>().color.r,
+                this.GetComponent<SpriteRenderer>().color.g,
+                this.GetComponent<SpriteRenderer>().color.b,
+                this.GetComponent<SpriteRenderer>().color.a - velocity * Time.deltaTime * 0.1f
+            );
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.localScale = new Vector3(0,0,1);
+            this.GetComponent<SpriteRenderer>().color = new Vector4(1,1,1,1);
+        }
+    }
+}
