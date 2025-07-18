eaaef1abc3245a3051df7da4d3ee20f2cd9e1f4c
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-07-18 18:38:44 -0300

    Erase python script for erase .meta files

--- eraseMetaFiles.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if file.endswith(".meta"):
-            path = os.path.join(root, file)
-            os.remove(path)
