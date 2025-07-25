557a4527546ecab7635002959dff2976b3174007
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-07-25 12:39:20 -0300

    movement

--- Assets/Scripts/Player/Player.gd
+++ Assets/Scripts/Player/Player.gd
@@ -1,14 +1,32 @@
 extends CharacterBody2D
 
 const SPEED = 200
+const RSPEED = 200
+const MAX_ANGLE_ROTATION = 15
 
-func _physics_process(_delta):
-	var input = 0
-	if Input.is_action_pressed("ui_left"):
-		input -= 1
-	if Input.is_action_pressed("ui_right"):
-		input += 1
-
+func movement(delta):
+	var x_movement := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	velocity.x = input * SPEED
+	var angle := rotation_degrees
+	if angle > 180:
+		angle -= 360
+
+	velocity.x = x_movement * SPEED
+	velocity.y = 0
+	move_and_slide()
+
+	if x_movement != 0.0:
+		if (angle > -MAX_ANGLE_ROTATION and x_movement < 0) or (angle < MAX_ANGLE_ROTATION and x_movement > 0):
+			rotation_degrees += x_movement * delta * RSPEED
+	else:
+		if abs(angle) > 1:
+			if angle > 0.0:
+				rotation_degrees -= delta * RSPEED
+			elif angle < 0.0:
+				rotation_degrees += delta * RSPEED
+		else:
+			angle = 0
+			
+func _physics_process(delta):
+	movement(delta)
 	move_and_slide()
