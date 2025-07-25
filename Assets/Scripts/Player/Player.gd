c2d60fcc737fde940a3aefa8f7bf8637957630ec
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-07-25 15:34:48 -0300

    Switch device

--- Assets/Scripts/Player/Player.gd
+++ Assets/Scripts/Player/Player.gd
@@ -8,6 +8,8 @@ const MAX_ANGLE_ROTATION  = 15
 const SMIWING_EFFECT_AMP  = 10
 const SMIWING_EFFECT_FREQ = 0.1
 
+const ANIMATION_EYES_DIST = 50
+
 func movement(delta):
 	var x_movement := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
@@ -33,9 +35,15 @@ func movement(delta):
 
 func smiwingEffect():
 	velocity.y = SMIWING_EFFECT_AMP*cos(2*PI*SMIWING_EFFECT_FREQ*Time.get_unix_time_from_system())
-	print(velocity.y)
+
+func animateEyes():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var direction = mouse_pos - global_position
+	var angle = direction.angle()
+	$Sprites/Head/Eyes.position = Vector2(cos(angle), sin(angle)) * ANIMATION_EYES_DIST
 
 func _physics_process(delta):
 	movement(delta)
 	smiwingEffect()
+	animateEyes()
 	move_and_slide()
