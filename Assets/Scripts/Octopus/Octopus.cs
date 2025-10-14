6dd65e5d15d749267021adc78fe25b3f4509910c
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-10-14 15:50:53 -0300

    ocotpus implementation

--- Assets/Scripts/Octopus/Octopus.cs
+++ Assets/Scripts/Octopus/Octopus.cs
@@ -6,6 +6,10 @@ public class Octopus : MonoBehaviour
     public GameObject OctopusRef;
     public float speed = 2f;
     private float angle = 0f;
+    public bool onShootArea = false;
+    public float rateFirePerSecond = 1.0f;
+    private float lastShoot = 0.0f;
+    public GameObject bullet;
 
     private void Start()
     {
@@ -20,5 +24,14 @@ public class Octopus : MonoBehaviour
         float y = Mathf.Sin(angle) * radius;
 
         transform.position = new Vector3(this.OctopusRef.transform.position.x + x, this.OctopusRef.transform.position.y + y, transform.position.z);
+
+        if(this.onShootArea)
+        {
+            if(Time.time > (this.lastShoot + (1.0/this.rateFirePerSecond)))
+            {
+                this.lastShoot = Time.time;
+                Instantiate(this.bullet, this.transform.position, this.transform.rotation);
+            }
+        }
     }
 }
