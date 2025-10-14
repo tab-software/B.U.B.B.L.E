9b2c793d9bdd3b279e53889befdb4dfa1523e7cb
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-10-14 15:51:02 -0300

    Octupus movement implementation

--- Assets/Scripts/Octopus/Octopus.gd
+++ Assets/Scripts/Octopus/Octopus.gd
@@ -1,1 +1,29 @@
-extends AnimatedSprite2D
+extends Node2D
+
+const TIME = 10
+
+@export var active = false
+var tween
+var lastDrop = 0.0
+
+func activate():
+	self.active = true
+
+func reset():
+	self.rotation_degrees = 45
+
+func finishTween():
+	self.reset()
+	self.active = false
+
+func _physics_process(delta):
+	if self.active:
+		if self.tween == null or (not self.tween.is_running()):
+			self.tween = create_tween()
+			self.tween.tween_property(self, "rotation_degrees", -45, self.TIME)
+			self.tween.finished.connect(func(): finishTween())
+		$Sprite.rotation_degrees = -rotation_degrees
+
+func _ready() -> void:
+	self.reset()
+	$Sprite.play()
