a82b4a7a72374077f88535ed1e9ce5bcf9b5582b
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-11-07 15:11:04 -0600

    Small changes

--- Assets/Scripts/Octopus/InkDroplet.gd
+++ Assets/Scripts/Octopus/InkDroplet.gd
@@ -8,7 +8,6 @@ var initialPosition
 func _ready() -> void:
 	$Area2D.add_to_group("InkDroplet")
 	self.initialPosition = self.global_position
-	print("ASd")
 
 func _process(delta: float) -> void:
 	self.position.y += delta * VEL
