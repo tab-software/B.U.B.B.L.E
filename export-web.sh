a7c1aa29c158ff88d1461bdd83fed1c5fa8be87e
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-11-08 03:01:04 -0300

    Update

--- export-web.sh
+++ export-web.sh
@@ -1,10 +1,11 @@
 git branch -D production
 git checkout --orphan production
-rm WebBuild -r
-mkdir WebBuild
-godot --path . --export-release "Web" ./WebBuild/index.html --headless
-git rm -rf .
-cp -r WebBuild/* .
+rm ./../WebBuild -r -f
+mkdir ./../WebBuild
+godot --path . --export-release "Web" ./../WebBuild/index.html --headless
+rf * -r -f
+cp -r ./../WebBuild/* .
+rm ./../WebBuild -r -f
 git add .
 git commit -m "Web production"
 git push https://github.com/tab-software/B.U.B.B.L.E/ production --force
